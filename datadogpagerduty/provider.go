7c0cfc49a12199f5b316ef1647a0c11e75ed5341
Andre Cloutier <andrecloutier@gmail.com>
2019-03-15 09:38:41 -0400

    Commit the rest

---
+++ datadogpagerduty/provider.go
@@ -0,0 +1,65 @@
+package datadogpagerduty
+
+import (
+	"errors"
+	"log"
+
+	"github.com/hashicorp/go-cleanhttp"
+	"github.com/hashicorp/terraform/helper/logging"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	datadog "github.com/zorkian/go-datadog-api"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("DATADOG_API_KEY", nil),
+			},
+			"app_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("DATADOG_APP_KEY", nil),
+			},
+			"api_url": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("DATADOG_HOST", nil),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"datadogpagerduty_service_integration": resourceDatadogIntegrationPagerdutyService(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	client := datadog.NewClient(d.Get("api_key").(string), d.Get("app_key").(string))
+	if apiURL := d.Get("api_url").(string); apiURL != "" {
+		client.SetBaseUrl(apiURL)
+	}
+
+	c := cleanhttp.DefaultClient()
+	c.Transport = logging.NewTransport("Datadog", c.Transport)
+	client.HttpClient = c
+
+	log.Println("[INFO] Datadog client successfully initialized, now validating...")
+	ok, err := client.Validate()
+	if err != nil {
+		log.Printf("[ERROR] Datadog Client validation error: %v", err)
+		return client, err
+	} else if !ok {
+		err := errors.New(`No valid credential sources found for Datadog Provider. Please see https://terraform.io/docs/providers/datadog/index.html for more information on providing credentials for the Datadog Provider`)
+		log.Printf("[ERROR] Datadog Client validation error: %v", err)
+		return client, err
+	}
+	log.Printf("[INFO] Datadog Client successfully validated.")
+
+	return client, nil
+}
